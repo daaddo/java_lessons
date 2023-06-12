f424198e87a0e94175b9535fe1e7a139930be0fb
davide <david.cascella.5@gmail.com>
2023-06-12 20:17:41 +0200

    costruttore, getter and setter di person

--- src/main/java/it/cascella/Person.java
+++ src/main/java/it/cascella/Person.java
@@ -6,6 +6,33 @@ public class Person {
     private String surname;
     private int age;
 
+    public Person(String name, String surname, int age) {
+        this.surname = surname;
+        this.age = age;
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
